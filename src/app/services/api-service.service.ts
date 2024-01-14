93ca5eaed7b7bb2a5a1af5ad50abb92923690224
devXodia <youstylez@gmail.com>
2024-01-14 01:18:48 +0100

    working on api calls

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, lastValueFrom} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiServiceService {
   constructor(private http: HttpClient) {}
+
+  // TYPE IN SERACH BAR THIS NAME: XΦDIA#1337 TO MAKE THE API CALL POSSIBLE. 
+
+  // URL IN BROWSER SHOULD BE: https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/XΦDIA/1337?api_key=RGAPI-65324bcf-0e42-41b5-80c7-5af7f3f6b490
 
   summonerName: string = '';
   tag: string = '';
@@ -27,3 +31,4 @@ export class ApiServiceService {
     }
   }
 }
+
