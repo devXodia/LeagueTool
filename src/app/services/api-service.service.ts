cbaa3fbdb6b28305613aff80a438d1628d49dcdb
devXodia <youstylez@gmail.com>
2024-01-16 21:43:18 +0100

    API TESTING

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -13,7 +13,7 @@ export class ApiServiceService {
   tag: string = '';
   puuid: string = '';
   accountJSON: any;
-  DEV_API_KEY: string = 'RGAPI-19f2f46a-b8cb-4b82-b302-47589b9440e2';
+  DEV_API_KEY: string = '';
 
   async getRiotId(summonerName: string, tag: string) {
     const url = `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${summonerName}/${tag}?api_key=${this.DEV_API_KEY}`;
