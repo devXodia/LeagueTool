6b6b4c63a6eb83cd840185ec93661051136c537a
devXodia <youstylez@gmail.com>
2024-01-19 14:38:44 +0100

    working on api calls

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -13,7 +13,7 @@ export class ApiServiceService {
   tag: string = '';
   puuid: string = '';
   accountJSON: any;
-  DEV_API_KEY: string = 'RGAPI-902f2ed0-566c-43ed-b2e4-80caa402d0cf';
+  DEV_API_KEY: string = '';
  
 
 
@@ -28,8 +28,15 @@ export class ApiServiceService {
     console.log('TEST', data)
     this.assignData(data, summonerName, tag);
     
-    
-    
+  }
+
+
+
+  assignData(data: AccountData, summonerName: string, tag: string) {
+    this.accountJSON = data;
+    this.puuid = data.puuid;
+    this.summonerName = summonerName;
+    this.tag = tag;
   }
 
 
@@ -37,17 +44,7 @@ export class ApiServiceService {
 
 
 
-
-
-
-
-/*   async getRankedData(){
-    const url = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${this.puuid}?api_key=${this.DEV_API_KEY}`;
-    const rankedData = await lastValueFrom(this.http.get<any>(url));
-    console.log('RANKED DATA', rankedData)
-    this.getMatchesData();
-  }
-
+/* 
   async getMatchesData(){
     const url = `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${this.puuid}/ids?api_key=${this.DEV_API_KEY}`;
     const matchData = await lastValueFrom(this.http.get<any>(url));
@@ -59,12 +56,8 @@ export class ApiServiceService {
     const url = `https://europe.api.riotgames.com/lol/match/v5/matches/${this.match}?api_key=${this.DEV_API_KEY}`;
     const match = await lastValueFrom(this.http.get<any>(url));
     console.log('MATCH DATA', match)
-  } */
+  }  */
 
-  assignData(data: AccountData, summonerName: string, tag: string) {
-    this.accountJSON = data;
-    this.puuid = data.puuid;
-    this.summonerName = summonerName;
-    this.tag = tag;
-  }
 }
+
+
